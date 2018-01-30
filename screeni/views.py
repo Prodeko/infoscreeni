9c66d163d251777d5b9ea1de28aae25dac1ca160
Kiikkilä Olli <olli.kiikkila@aalto.fi>
2018-01-30 18:52:01 +0200

    Basic models, templates structure, url and a default view

--- screeni/views.py
+++ screeni/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from screeni.models import *
 
 # Create your views here.
+
+def index(request):
+    #return HttpResponse("Jes")
+    return render(request, "index.html", {})
