2624998f2f16f233813547c26a8a15b1b9cc2aa0
Kiikkilä Olli <olli.kiikkila@aalto.fi>
2018-02-05 22:34:12 +0200

    Models muutoksia

--- screeni/models.py
+++ screeni/models.py
@@ -9,6 +9,11 @@ class PromoSlide(models.Model):
     application_deadline = models.DateField(blank=True, null=True)
     image = models.FileField(upload_to='promo_images/', null=True)
 
+    def get_picture_url(self):
+        if self.picture:
+            return self.picture.url
+        return "/static/images/no_profile.jpg"
+
 class ContentSlide(models.Model):
     title = models.CharField(max_length=255)
     ingress = models.CharField(null=True, max_length=255)
