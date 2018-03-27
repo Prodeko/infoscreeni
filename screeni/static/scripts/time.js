42b9f9005d43caada98000a509570f9dfd566d25
rriski <timo@totaltrivia.com>
2018-03-27 22:34:03 +0300

    Fix date formatting in the upper left corner clock

--- screeni/static/scripts/time.js
+++ screeni/static/scripts/time.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
   /* Credits: https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing_clock */
 
   function startTime() {
-    var today = new Date();
-    var date = today.toLocaleDateString();
-    var time = today.toLocaleTimeString();
+    var date = moment().format('D.M.YYYY');
+    var time = moment().format('h:mm:ss');
     $('#date').html(date);
     $('#time').html(time);
     var t = setTimeout(startTime, 1000);
