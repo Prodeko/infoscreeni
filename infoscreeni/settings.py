f65ea8b50800e228de9f575bd61f6b202f704300
Leo Drosdek <drosdek.leo@gmail.com>
2018-03-28 19:55:38 +0300

    Configure HTTPS and WSS, and fix a few things

--- infoscreeni/settings.py
+++ infoscreeni/settings.py
@@ -20,7 +20,7 @@ SITE_ID = 1
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Media directory for Promo file uploads
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://infoscreen.prodeko.org/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
@@ -38,9 +38,9 @@ TRELLO_TOKEN = config['TRELLO']['TOKEN']
 GIPHY_KEY = config['GIPHY']['KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -135,8 +135,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = 'screeni//static'
+STATIC_URL = 'https://infoscreen.prodeko.org/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # CKEditor settings
