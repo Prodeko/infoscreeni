ae3954b666a2790d23103c86211550ceb20eadea
rriski <timo.h.riski@outlook.com>
2018-10-04 15:40:31 +0300

    Test push after moving repository to prodeko org

--- infoscreeni/routing.py
+++ infoscreeni/routing.py
@@ -1,7 +1,6 @@
-from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 from screeni.consumers import SlideConsumer
-from django.core.wsgi import get_wsgi_application
 
 application = ProtocolTypeRouter({
     "websocket":
